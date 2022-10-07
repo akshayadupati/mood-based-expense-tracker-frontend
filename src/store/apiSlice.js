9599e80c8995dc5805e7353903c179739a073ca0
Akshaya Dupati <98553741+akshayadupati@users.noreply.github.com>
2022-10-07 15:50:50 -0400

    Update apiSlice.js

--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseURI = "http://localhost:8080";
+const baseURI = "https://mood-expense-tracker-app.herokuapp.com";
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: baseURI }),
   endpoints: (builder) => ({
